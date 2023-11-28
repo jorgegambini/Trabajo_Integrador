5a74b63b9e317c5b4f17c857f711884564dddda1
JGambix71 <80544921+jorgegambini@users.noreply.github.com>
2023-11-27 23:03:03 -0300

    Update IngresarFecha.java

--- src/main/java/ar/com/jg/view/accessories/IngresarFecha.java
+++ src/main/java/ar/com/jg/view/accessories/IngresarFecha.java
@@ -16,46 +16,7 @@ import java.util.Date;
 
 public class IngresarFecha extends JPanel {
 
-    private DateChooser dateChooser;
-    private JTextField textoField;
-    private JButton cmdDate;
-    @Getter @Setter
-    private LocalDate dateField;
-
-    public IngresarFecha(String textLabel, LocalDate date, int longLabel, int longField, int gap) {
-
-        setLayout(new MigLayout("wrap,fillx,insets 5 10 5 10", "[fill,"+ (longLabel + longField + 80)+"]"));
-
-        JLabel textoLabel = new JLabel(textLabel);
-        textoField = new JTextField();
-        dateChooser = new DateChooser();
-        cmdDate = new JButton("...");
-
-        dateChooser.setDateSelectionMode(DateChooser.DateSelectionMode.SINGLE_DATE_SELECTED);
-        dateChooser.setDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
-        dateChooser.setSelectedDate(Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant()));
-        dateChooser.setTextField(textoField);
-        textoField.setEnabled(false);
-        textoField.setHorizontalAlignment(SwingConstants.RIGHT);
-
-        add(textoLabel, "split 3, width " + longLabel + ":" + longLabel + ":" + longLabel + ", growx");
-        add(textoField, "width " + longField + ":" + longField + ":" + longField);
-        add(cmdDate,"width 30:30:30, height 20:20:20, pushx, gapright " + gap);
-
-        dateChooser.addActionDateChooserListener(new DateChooserAdapter() {
-            @Override
-            public void dateChanged(Date date, DateChooserAction action) {
-
-                dateField = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
-            }
-        });
-
-        cmdDate.addActionListener(e -> dateChooser.showPopup());
-
-        dateField = date;
-
-        dateChooser.addAncestorListener(new RequestFocusListener());
+    public IngresarFecha() {
 
     }
 
