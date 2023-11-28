1b858d4e6239b5e8751c0e8fae825e99bfc4ca0c
JGambix71 <80544921+jorgegambini@users.noreply.github.com>
2023-11-28 12:57:44 -0300

    Update ReporteIncidenciaRepository.java

--- src/main/java/ar/com/jg/repositories/ReporteIncidenciaRepository.java
+++ src/main/java/ar/com/jg/repositories/ReporteIncidenciaRepository.java
@@ -27,14 +27,6 @@ public class ReporteIncidenciaRepository implements CrudRepository<ReporteIncide
 
     }
 
-    public List<ReporteIncidencia> listarPorRangoFecha(LocalDate fechaDesde, LocalDate fechaHasta) {
-
-        EstadoProblema estadoProblema = EstadoProblema.Resuelto;
-
-        return em.createQuery("from ReporteIncidencia r where r.estadoProblema = :estadoProblema and r.fechaAlta between :fechaDesde and :fechaHasta", ReporteIncidencia.class).setParameter("estadoProblema", estadoProblema).setParameter("fechaDesde", fechaDesde).setParameter("fechaHasta", fechaHasta).getResultList();
-
-    }
-
     @Override
     public ReporteIncidencia buscarPorId(Long id) {
 
@@ -42,64 +34,16 @@ public class ReporteIncidenciaRepository implements CrudRepository<ReporteIncide
 
     }
 
-    public ReporteIncidencia buscarPorCodigo(String codigoReporte) {
-
-        EstadoProblema estadoProblema1 = EstadoProblema.Cancelado;
-        EstadoProblema estadoProblema2 = EstadoProblema.Resuelto;
-
-        try {
-
-            return em.createQuery("from ReporteIncidencia r where r.codigoReporte like :codigoReporte and r.estadoProblema != :estadoProblema1 and r.estadoProblema != :estadoProblema2", ReporteIncidencia.class).setParameter("codigoReporte", codigoReporte).setParameter("estadoProblema1", estadoProblema1).setParameter("estadoProblema2", estadoProblema2).getSingleResult();
-
-        } catch (NoResultException e) {
-
-            return null;
-
-        }
-
-    }
-
     @Override
     public void guardar(ReporteIncidencia reporteIncidencia) {
 
-        String reportCode = RandomString.getAlphaNumericString(10);
-        String lblReporte = "";
-
         if (reporteIncidencia.getId() != null && reporteIncidencia.getId() > 0) {
-
-            String estado = reporteIncidencia.getEstadoProblema().name();
-
-            reporteIncidencia.setCodigoReporte(reportCode);
 
             em.merge(reporteIncidencia);
 
-            if (estado.equals("Cancelado")) {
-
-                lblReporte = "<html><div style=\"text-align: center;\">El Reporte de Incidencia se ha " + estado + " correctamente.</div><br>" +
-                        "<div style=\"text-align: center;\"> <h1 style=\"font-size: 24px; color: red;\">" + reportCode + "</h1></div></html>";
-
-                MostrarLabel mostrarLabel = new MostrarLabel(lblReporte, 350, 50);
-                JOptionPane.showOptionDialog(null, mostrarLabel, "Reporte Incidencia Actualizado", JOptionPane.OK_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Aceptar"}, "Aceptar");
-
-            } else {
-
-                JOptionPane.showMessageDialog(null, "El Reporte Incidencia se ha actualizado correctamente.");
-
-            }
-
-
-
         } else {
 
-            reporteIncidencia.setCodigoReporte(reportCode);
-
             em.persist(reporteIncidencia);
-
-            lblReporte = "<html><div style=\"text-align: center;\">El Reporte de Incidencia se ha ingresado correctamente.</div><br>" +
-                    "<div style=\"text-align: center;\"> <h1 style=\"font-size: 24px; color: red;\">" + reportCode + "</h1></div></html>";
-
-            MostrarLabel mostrarLabel = new MostrarLabel(lblReporte, 320, 50);
-            JOptionPane.showOptionDialog(null, mostrarLabel, "Reporte Incidencia Generado", JOptionPane.OK_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Aceptar"}, "Aceptar");
 
         }
 
